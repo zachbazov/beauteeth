907b18bda9b9b135e1375c01fb7c37a10a5d750e
Developer <developer@Developers-MBP.lan>
2023-07-10 13:30:59 +0300

    updated interface.

--- public/js/main.js
+++ public/js/main.js
@@ -35,8 +35,16 @@ navLink.forEach((n) => n.addEventListener('click', linkAction));
 function scrollHeader() {
   const header = document.getElementById('header');
   // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 50) header.classList.add('scroll-header');
-  else header.classList.remove('scroll-header');
+  if (this.scrollY >= 50) {
+    header.classList.remove('scroll-header');
+    header.classList.add('scroll-header-white');
+  } else if (this.scrollY >= 768 - 72) {
+    header.classList.remove('scroll-header-white');
+    header.classList.add('scroll-header');
+  } else if (this.scrollY == 0) {
+    header.classList.remove('scroll-header-white');
+    header.classList.remove('scroll-header');
+  }
 }
 window.addEventListener('scroll', scrollHeader);
 
